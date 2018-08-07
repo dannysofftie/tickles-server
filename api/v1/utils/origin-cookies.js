d8d85b5a6f6f52cce71b6f0b2e5e5dfd5467472b
dannysofftie <dankim761@gmail.com>
2018-08-07 13:36:54 +0300

    Extract publisher cookies

--- api/v1/utils/origin-cookies.js
+++ api/v1/utils/origin-cookies.js
@@ -7,16 +7,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 function extractRequestCookies(cookieString, cookieName) {
     if (cookieString == undefined)
-        return false;
+        return;
     let c = decodeURIComponent(cookieString), d, e = {};
     if (c.indexOf(';') != -1)
         d = c.split(';');
     else
         d = c;
     if (typeof d == 'string')
-        e[d.split('=')[0]] = d.split('=')[1];
+        e[d.split('=')[0].trim()] = d.split('=')[1].trim();
     else
-        d.map(p => e[p.split('=')[0]] = p.split('=')[1]);
+        d.map(p => e[p.split('=')[0].trim()] = p.split('=')[1].trim());
     if (typeof cookieName != 'undefined')
         return e[cookieName];
     return e;
--- src/api/v1/utils/origin-cookies.ts
+++ src/api/v1/utils/origin-cookies.ts
@@ -6,16 +6,16 @@
  */
 export function extractRequestCookies(cookieString: string | Array<string>, cookieName?: string) {
     if (cookieString == undefined)
-        return false
+        return
     let c = decodeURIComponent(<string>cookieString),
         d: string | Array<string>, e: object = {}
 
     if (c.indexOf(';') != -1) d = c.split(';')
     else d = c
     if (typeof d == 'string')
-        e[d.split('=')[0]] = d.split('=')[1]
+        e[d.split('=')[0].trim()] = d.split('=')[1].trim()
     else
-        d.map(p => e[p.split('=')[0]] = p.split('=')[1])
+        d.map(p => e[p.split('=')[0].trim()] = p.split('=')[1].trim())
 
     if (typeof cookieName != 'undefined')
         return e[cookieName]
